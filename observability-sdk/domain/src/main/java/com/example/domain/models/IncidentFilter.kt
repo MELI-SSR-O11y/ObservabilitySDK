44c81358391257fb8cbb11f16fbaeaa1fb1e9ee9
BFCalderon <brayandelta123@hotmail.com>
2026-01-04 02:17:24 -0500

    i18n: Localize time filter display names to Spanish
    Translates the `displayName` strings for the `TimeFilter` sealed class into Spanish. This change affects the UI where these time filter options are presented to the user.
    
    Key changes:
    
    -   `"All Time"` is now `"Desde el inicio"`.
    -   `"Last Minute"` is now `"Ultimo Minuto"`.
    -   `"Last 5 Minutes"` is now `"Ultimos 5 minutos"`.
    -   `"Last 30 Minutes"` is now `"Ultimos 30 minutos"`.
    -   `"Last 12 Hours"` is now `"Ultimas 5 horas"`.
    -   `"Last 24 Hours"` is now `"Ultimas 24 horas"`.

--- observability-sdk/domain/src/main/java/com/example/domain/models/IncidentFilter.kt
+++ observability-sdk/domain/src/main/java/com/example/domain/models/IncidentFilter.kt
@@ -15,12 +15,12 @@ data class IncidentFilter(
  * Sealed class representing the different time filter options.
  */
 sealed class TimeFilter(val durationMillis: Long, val displayName: String) {
-    object None : TimeFilter(0, "All Time")
-    object LastMinute : TimeFilter(1 * 60 * 1000, "Last Minute")
-    object Last5Minutes : TimeFilter(5 * 60 * 1000, "Last 5 Minutes")
-    object Last30Minutes : TimeFilter(30 * 60 * 1000, "Last 30 Minutes")
-    object Last12Hours : TimeFilter(12 * 60 * 60 * 1000, "Last 12 Hours")
-    object Last24Hours : TimeFilter(24 * 60 * 60 * 1000, "Last 24 Hours")
+    object None : TimeFilter(0, "Desde el inicio")
+    object LastMinute : TimeFilter(1 * 60 * 1000, "Ultimo Minuto")
+    object Last5Minutes : TimeFilter(5 * 60 * 1000, "Ultimos 5 minutos")
+    object Last30Minutes : TimeFilter(30 * 60 * 1000, "Ultimos 30 minutos")
+    object Last12Hours : TimeFilter(12 * 60 * 60 * 1000, "Ultimas 5 horas")
+    object Last24Hours : TimeFilter(24 * 60 * 60 * 1000, "Ultimas 24 horas")
 
     companion object {
         fun allFilters() = listOf(None, LastMinute, Last5Minutes, Last30Minutes, Last12Hours, Last24Hours)
